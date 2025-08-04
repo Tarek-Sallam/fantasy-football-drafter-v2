b04bdaf2b2f293a8e05da9180b544e24b1ff731a
Tarek-Sallam <tarek.i.sallam@gmail.com>
2025-08-04 13:16:18 -0400

    added caching + player data loading to playerscraper

--- fantasydrafter/data/PlayerScraper.py
+++ fantasydrafter/data/PlayerScraper.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 from typing import Optional
 import logging
 import time
-from typing import Set, Tuple
 import string
+import pandas as pd
+from pathlib import Path
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -19,6 +20,7 @@ class PlayerScraper:
     def __init__(self, year: int = 2025, delay: float = 1.0):
         self.year = year
         self.delay = delay
+        self.players = pd.DataFrame(columns=["Name", "Link"])
         self.session = requests.Session()
         self.session.headers.update(self.HEADERS)
 
@@ -32,9 +34,8 @@ class PlayerScraper:
             logger.error(f"Error making request to {url}: {e}")
             return None
     
-    def get_players_by_letter(self, letter: str) -> Set[Tuple[str, str]]:
+    def get_players_by_letter(self, letter: str) -> None:
         url = f"{self.BASE_URL}/{letter.upper()}/"
-        players = set()
         try:
             soup = self._make_request(url)
             player_content = soup.find("div", {"id": "all_players"}).find("div", {"id": "div_players"})
@@ -43,15 +44,43 @@ class PlayerScraper:
                 if start_year <= self.year <= end_year:
                     href = player.find("a").get("href")
                     name = player.fin("a").text
-                    players.add((name, href))
-        
-            return players
+                    self.players = self.players.append({"Name": name, "Link": href}, ignore_index=True)
+            logger.info(f"Found {len(self.players)} players for letter {letter}")
+
         except Exception as e:
             logger.error(f"Error getting players by letter {letter}: {e}")
-            return players
         
-    def get_all_players(self) -> Set[Tuple[str, str]]:
-        players = set()
-        for letter in string.ascii_lowercase:
-            players.update(self.get_players_by_letter(letter))
-        return players
+    def get_all_players(self) -> None:
+        try:
+            for letter in string.ascii_lowercase:
+                self.get_players_by_letter(letter)
+
+            logger.info(f"Found {len(self.players)} players for {self.year} season")
+        except Exception as e:
+            logger.error(f"Error getting all players: {e}")
+
+    def cache_players(self, cach_dir: Path) -> None:
+        try:
+            self.players.to_csv(cach_dir / f"playerscraper_playerlinks_{self.year}.csv", index=False)
+            del self.players
+            logger.info(f"Cached {len(self.players)} players to {cach_dir / f"playerscraper_playerlinks_{self.year}.csv"}")
+        except Exception as e:
+            logger.error(f"Error caching players: {e}")
+
+    def get_player_data(self, player_url: str, cache_dir: Path = None) -> None:
+        try:
+            if cache_dir is not None:
+                player_df = pd.read_csv(cache_dir / f"playerscraper_playerlinks_{self.year}.csv")
+            else:
+                player_df = self.players
+            
+            for name, link in player_df["Name"], player_df["Link"]:
+                url = f"{self.BASE_URL}/{link}/"
+                soup = self._make_request(url)
+                
+                
+        except Exception as e:
+            logger.error(f"Error getting player data: {e}")
+            
+
+            
